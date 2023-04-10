02acffb7535d4d7ee6f6bc4300dca3a231982720
Jubair Ahammed Tolon <zs.tolon0073@gamil.com>
2023-04-11 01:56:14 +0600

    array.form()

--- Problems/DOM-manipulation.js
+++ Problems/DOM-manipulation.js
@@ -121,3 +121,58 @@ function deleteHeading() {
     console.log(h1)
     h1.remove();
 }
+
+
+console.log(Array.from('foo'));
+// Expected output: Array ["f", "o", "o"]
+
+console.log(Array.from([1, 2, 3], x => x + x));
+// Expected output: Array [2, 4, 6]
+
+
+const set = new Set(["foo", "bar", "baz", "foo", "baz"]);
+Array.from(set);
+console.log(set)
+// [ "foo", "bar", "baz" ]
+
+
+
+const map = new Map([
+    [1, 2],
+    [2, 4],
+    [4, 8],
+]);
+Array.from(map);
+// [[1, 2], [2, 4], [4, 8]]
+
+const mapper = new Map([
+    ["1", "a"],
+    ["2", "b"],
+]);
+Array.from(mapper.values());
+// ['a', 'b'];
+
+Array.from(mapper.keys());
+// ['1', '2'];
+
+
+Array.from({ length: 5 }, (v, i) => i);
+// [0, 1, 2, 3, 4]
+
+
+const range = (start, stop, step) =>
+    Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step);
+
+// Generate numbers range 0..4
+range(0, 4, 1);
+// [0, 1, 2, 3, 4]
+
+// Generate numbers range 1..10 with step of 2
+range(1, 10, 2);
+// [1, 3, 5, 7, 9]
+
+// Generate the alphabet using Array.from making use of it being ordered as a sequence
+range("A".charCodeAt(0), "Z".charCodeAt(0), 1).map((x) =>
+    String.fromCharCode(x),
+);
+// ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
