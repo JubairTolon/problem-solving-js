5c0c9be75c258c31eed1a09f682eab4dcf418ba6
Jubair Ahammed Tolon <zs.tolon0073@gamil.com>
2023-07-17 15:02:43 +0600

    word center

--- Problems/Encryption.js
+++ Problems/Encryption.js
@@ -28,3 +28,38 @@ console.log(typeof (addSpace(str2))); //string
 
 /******* Find the center of a word ***********/
 
+function findCenter(sample) {
+    let centerSpaceWord = '';
+    let center = 0;
+
+    if (sample.length % 2 === 0) {
+        center = sample.length / 2;
+        // console.log(center);
+        for (let i = 0; i < sample.length; i++) {
+            if (i === center - 1) {
+                centerSpaceWord += sample[i] + ' ';
+            }
+            else {
+                centerSpaceWord += sample[i];
+            }
+        }
+    }
+    else {
+        center = Math.floor(sample.length / 2);
+        console.log('This ia an odd number word! The center of this word is: ', sample[center]);
+        for (let i = 0; i < sample.length; i++) {
+            if (i === center) {
+                centerSpaceWord += ' ';
+            }
+            else {
+                centerSpaceWord += sample[i];
+            }
+        }
+        console.log('Word without center letter: ', centerSpaceWord);
+    }
+    return centerSpaceWord;
+}
+const sampleWord1 = 'banana';
+const sampleWord2 = 'mango';
+// console.log(findCenter(sampleWord1));
+console.log(findCenter(sampleWord2));
